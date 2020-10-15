9f41cc57958d87d2fd91536c1ca8c2b706865ba0
Vermath <65049377+Vermath@users.noreply.github.com>
2020-10-15 13:46:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -47,6 +47,21 @@ def plots():
     # Return template and data
     return render_template("plots.html", off_db=off_list, def_db=def_list, misc_db=misc_list)
 
+@app.route("/greensock")
+def greensock():
+    # Find one record of data from the mongo database
+    off_data = offense.find()
+    off_list = list(off_data)
+
+    def_data = defense.find()
+    def_list = list(def_data)
+
+    misc_data = misc.find()
+    misc_list = list(misc_data)
+    
+    # Return template and data
+    return render_template("plots.html", off_db=off_list, def_db=def_list, misc_db=misc_list)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/logic.js
+++ static/logic.js
@@ -39,21 +39,86 @@ console.log(def_data);
 console.log(misc_data);
 console.log("hi");
 
+//Nest function performs a group by on the data and gives us the mean for each year
+// first is passing yards
 var off_data_moe = d3.nest()
   .key(function(d) { return d.years; })
-  .rollup(function(v) {return d3.mean(v, function(d) {return d.margin_of_victory; }); })
+  .rollup(function(v) {return d3.mean(v, function(d) {return d.pass_yards; }); })
   .entries(off_data);
 
 console.log(JSON.stringify(off_data_moe));
 
-var mar_of_vic = off_data_moe.map(x => x.value)
-var years = off_data_moe.map(x => x.key)
+var y = off_data_moe.map(x => x.value)
+var x = off_data_moe.map(x => x.key)
 
+//passing yards scatter
 var trace1 = {
-    x: years,
-    y: mar_of_vic,
+    x: x,
+    y: y,
     type: "scatter"
   };
 
-var data = [trace1];
-Plotly.newPlot("plot", data);
+var data1 = [trace1];
+Plotly.newPlot("plot", data1);
+//Second is passing touchdowns
+var off_data_joe = d3.nest()
+  .key(function(d) { return d.years; })
+  .rollup(function(v) {return d3.mean(v, function(d) {return d.pass_touchdowns; }); })
+  .entries(off_data);
+
+console.log(JSON.stringify(off_data_joe));
+
+var y1 = off_data_joe.map(x => x.value)
+var x1 = off_data_joe.map(x => x.key)
+
+//Passing touchdowns scatter
+var trace2 = {
+    x: x1,
+    y: y1,
+    type: "scatter"
+  };
+
+var data2 = [trace2];
+Plotly.newPlot("plot1", data2);
+
+//third is total yards
+var off_data_doe = d3.nest()
+  .key(function(d) { return d.years; })
+  .rollup(function(v) {return d3.mean(v, function(d) {return d.yards; }); })
+  .entries(off_data);
+
+console.log(JSON.stringify(off_data_doe));
+
+var y2 = off_data_doe.map(x => x.value)
+var x2 = off_data_doe.map(x => x.key)
+
+//yards scatter
+var trace3 = {
+    x: x2,
+    y: y2,
+    type: "scatter"
+  };
+
+var data3 = [trace3];
+Plotly.newPlot("plot2", data3);
+
+//fourth is rushing yards
+var off_data_boe = d3.nest()
+  .key(function(d) { return d.years; })
+  .rollup(function(v) {return d3.mean(v, function(d) {return d.rush_yards; }); })
+  .entries(off_data);
+
+console.log(JSON.stringify(off_data_boe));
+
+var y3 = off_data_boe.map(x => x.value)
+var x3 = off_data_boe.map(x => x.key)
+
+//rushing yards scatter
+var trace4 = {
+    x: x3,
+    y: y3,
+    type: "scatter"
+  };
+
+var data4 = [trace4];
+Plotly.newPlot("plot3", data4);
