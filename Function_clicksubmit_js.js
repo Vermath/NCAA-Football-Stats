79d1ddc9aaba5b413f386a6d165c43b088ed745e
Dave D <66148262+dadebrows@users.noreply.github.com>
2020-10-17 00:03:46 -0400

    Add files via upload

---
+++ Function_clicksubmit_js.js
@@ -0,0 +1,14 @@
+function clicksubmit(){
+
+    VoteGif1 = document.getElementById('Gif1'); 
+    alert("You voted for "+ Gif1);
+    VoteGif2 = document.getElementById('Gif2'); 
+    alert("You voted for "+ Gif2);
+    VoteGif2 = document.getElementById('Gif3'); 
+    alert("You voted for "+ Gif3);
+    VoteGif2 = document.getElementById('Gif4'); 
+    alert("You voted for "+ Gif4);
+    VoteGif2 = document.getElementById('Gif5'); 
+    alert("You voted for "+ Gif5);
+
+}
